f7b8f5a4c9a61e209e02043734cd3ae738e80eb8
Tine <tinemeretesikjaerpetersen@gmail.com>
2024-07-29 21:47:48 +0200

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './global.css';
+import { Routes, Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+        <h1 className='text-3xl font-bold underline'>
+          Bugging It
+        </h1>
+    </main>
+  )
+}
+
+export default App
