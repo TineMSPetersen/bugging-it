51c8a3b3064c409b7c22203fcec32e81869b91f3
TineMSPetersen <111304307+TineMSPetersen@users.noreply.github.com>
2024-08-03 15:09:06 +0200

    Create bottombar
    Bottombar for mobile navigation

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -28,14 +28,19 @@ export const sidebarLinks = [
 
 export const bottombarLinks = [
   {
-    imgURL: "/assets/icons/home.svg",
-    route: "/",
-    label: "Home",
-  },
-  {
     imgURL: "/assets/icons/wallpaper.svg",
     route: "/explore",
     label: "Explore",
+  },
+  {
+    imgURL: "/assets/icons/people.svg",
+    route: "/all-users",
+    label: "People",
+  },
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
   },
   {
     imgURL: "/assets/icons/bookmark.svg",
