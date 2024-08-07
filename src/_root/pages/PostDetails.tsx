294d9a8f7b13a6c6f53bb4161a13751bf0510f7b
TineMSPetersen <111304307+TineMSPetersen@users.noreply.github.com>
2024-08-07 22:26:33 +0200

    Post Details finish

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,11 +1,21 @@
 import Loader from '@/components/shared/Loader';
-import { UseGetPostById } from '@/lib/react-query/queriesAndMutations'
+import PostStats from '@/components/shared/PostStats';
+import { Button } from '@/components/ui/button';
+import { useUserContext } from '@/context/AuthContext';
+import { UseDeletePost, UseGetPostById } from '@/lib/react-query/queriesAndMutations'
 import { formatDateString } from '@/lib/utils';
 import { Link, useParams } from 'react-router-dom'
 
 const PostDetails = () => {
   const { id } = useParams();
   const { data: post, isPending } = UseGetPostById(id || '');
+  const { user } = useUserContext();
+  const { mutate: deletePost } = UseDeletePost();
+
+  const handleDeletePost = () => {
+    deletePost({ postId: id, imageId: post?.imageId });
+    navigate(-1);
+  }
   
   return (
     <div className='post_details-container'>
@@ -32,20 +42,53 @@ const PostDetails = () => {
               <div className='flex flex-col'>
                 <a className='base-medium lg:body-bold text-light-1' href={`/profile/${post?.creator.$id}`}>{post?.creator.name}</a>
           
-                <div className='flex-center gap-2 text-light-3'>
+                <div className='text-light-3'>
                   <p className='subtle-semibold lg:small-regular'>
                     {formatDateString(post?.$createdAt)}
                   </p>
-                  -
                   <p className='subtle-semibold lg:small-regular'>
                   {post?.location}
                   </p>
                 </div>
               </div>
-              
+            </div> 
+              <div className='flex-center gap-4'>
+                <Link
+                  to={`/update-post/${post?.$id}`}
+                  className={`${user.id !== post?.creator.$id && 'hidden'}`}
+                >
+                  <img
+                    src="/assets/icons/edit.svg"
+                    alt="edit post"
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+
+                <Button
+                  onClick={handleDeletePost}
+                  className={`ghost_details-delete_btn ${user.id !== post?.creator.$id && 'hidden'}`}
+                >
+                  <img src="/assets/icons/delete.svg" alt="delete post" width={24} height={24} />
+                </Button>
+              </div>
             </div>
-          </div>
-          <div className='flex-center gap-4'></div>
+
+            <hr className='border w-full border-dark-4/80' />
+            <div className='flex flex-col flex-1 w-full small-medium lg:base-regular'>
+              <p>{post?.caption}</p>
+              <ul className='flex gap-2 mt-2'>
+                {post?.tags.map((tag: string) => (
+                <li key={tag} className='text-light-3'>
+                  #{tag}
+                </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className='w-full'>
+              <PostStats post={post} userId={user.id} />
+            </div>
           </div>
         </div>
       )}
